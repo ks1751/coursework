dee4d883ba1f8338013da2a0ec47b626f54b468a
ks1751 <ks1751@live.mdx.ac.uk>
2024-11-28 15:00:48 +0100

    adding express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const {MongoClient, ObjectId} = require('mongodb');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
